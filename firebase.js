743d219bdac4c39405a1d36e4c91ab4f793c8357
SakshiiDev <ssakshiidev@gmail.com>
2025-05-12 15:45:54 +0530

    Initial commit with HTML, CSS, JS, Firebase project

---
+++ firebase.js
@@ -0,0 +1,16 @@
+// // firebase.js
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyCr8_VanuHDfUE_UOi9qcOzP0moaNvuTj4",
+//     authDomain: "the-gurukul-edu.firebaseapp.com",
+//     projectId: "the-gurukul-edu",
+//     storageBucket: "the-gurukul-edu.firebasestorage.app",
+//     messagingSenderId: "122191740051",
+//     appId: "1:122191740051:web:0e8cf824db63136f80e166",
+//     measurementId: "G-3YZDCZ8X1X"
+//   };
+
+// export const app = initializeApp(firebaseConfig);
+// export const analytics = getAnalytics(app);
